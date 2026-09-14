2dc6e3ea03d3aef89b536d113f0f3d24adc74370
worldofgatlan0-ui <worldofgatlan0@gmail.com>
2026-09-14 19:08:00 +0930

    Add files via upload

---
+++ UNDERTALE-OUTERHAVEN/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://pyeigukdadfmlyeyeljv.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_oB9KcvqADqbelWpW7YmP5Q_-X7ugn82";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
